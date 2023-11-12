fe28df7c65c9eecb0f1ac03dab4d62146eb5de4d
LeeHyunsoo <mm083434@gmail.com>
2023-11-12 23:44:55 +0900

    First Commit

---
+++ Calculator/RoundButton.swift
@@ -0,0 +1,20 @@
+//
+//  RoundButton.swift
+//  Calculator
+//
+//  Created by HyunSoo on 11/11/23.
+//
+
+import UIKit
+
+class RoundButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
